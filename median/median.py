bc95f43cec29bfc809226fe06ae6c920f604ddce
delhivery-rajesh <rajesh.verma1@delhivery.com>
2018-09-20 13:24:33 +0530

    changes file structure

---
+++ median/median.py
@@ -0,0 +1,8 @@
+mport statistics
+
+def median(a):
+    """
+    This method retur median of given input array.
+    """
+    return statistics.median(a)
+
--- setup.py
+++ setup.py
@@ -5,24 +5,14 @@ def readme():
     with open('README.rst') as f:
         return f.read()
 
-setup(name='nkrajesh',
-        version='0.1',
-        description = 'Return median of given list',
-        long_description = readme(),
-        classifiers = ['Development Status :: 3- Alpha',
-            'License :: OSI Approved :: MIT License',
-            'Programming Language:: Python::3.5',
-            'Topic:: Text Processing :: Linguistic',
-            ],
-        keywords='median of given array list',
-        url = 'http://',
-        author = 'rajesh',
-        author_email = 'rjnkvrmrjsh@gmail.com',
-        license = 'MIT',
-        packages = ['nkrajesh'],
-        install_requires = ['statistics'],
-        test_suite = 'nose.collector',
-        test_require = ['nose', 'nose-cover3'],
-        include_package = True,
-        zip_safe = False
-        )
+setup(
+    name='pkh-median',
+    version='0.1',
+    license='MIT',
+    description='An example python package median',
+    long_description=open('README.txt').read(),
+    install_requires=['statistics'],
+    url='https://github.com/rajeshiiti/median.git',
+    author='Rajesh Verma',
+    author_email='rjnkvrmrjsh@gmail.com'
+)
