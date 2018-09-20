be2fd227e5503ae1aa17b86e43f6f60dbf152755
delhivery-rajesh <rajesh.verma1@delhivery.com>
2018-09-20 13:43:14 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
-from setuptools import setup 
+from setuptools import setup
 
 
 def readme():
     with open('README.rst') as f:
         return f.read()
-
 setup(
-    name='pkh-median',
+    name='median',
     version='0.1',
     license='MIT',
     description='An example python package median',
-    long_description=open('README.txt').read(),
+    long_description=open('README.rst').read(),
     install_requires=['statistics'],
     url='https://github.com/rajeshiiti/median.git',
     author='Rajesh Verma',
